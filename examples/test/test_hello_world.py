386cae4676769accb7092a2115bdda4e9f7dc23c
yaythomas <tgaigher@amazon.com>
2025-09-26 10:21:52 -0700

    fix: example test
    Example test was failing because return type now deserialized.

--- examples/test/test_hello_world.py
+++ examples/test/test_hello_world.py
@@ -9,13 +9,10 @@ from aws_durable_execution_sdk_python_testing.runner import (
 from src import hello_world
 
 
-class TestExamples:
-    """Integration tests for examples."""
+def test_hello_world():
+    """Test hello world example."""
+    with DurableFunctionTestRunner(handler=hello_world.handler) as runner:
+        result: DurableFunctionTestResult = runner.run(input="test", timeout=10)
 
-    def test_hello_world(self):
-        """Test hello world example."""
-        with DurableFunctionTestRunner(handler=hello_world.handler) as runner:
-            result: DurableFunctionTestResult = runner.run(input="test", timeout=10)
-
-        assert result.status is InvocationStatus.SUCCEEDED  # noqa: S101
-        assert result.result == '"Hello World!"'  # noqa: S101
+    assert result.status is InvocationStatus.SUCCEEDED  # noqa: S101
+    assert result.result == "Hello World!"  # noqa: S101
